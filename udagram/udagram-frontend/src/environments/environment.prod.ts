6aeb3fb983fd1b34503130dec79fa9b09b026f02
Pedro Leal <lealpedro2009@gmail.com>
2025-07-09 10:51:54 +0100

    add frontend url to circleci env variables

--- udagram/udagram-frontend/src/environments/environment.prod.ts
+++ udagram/udagram-frontend/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: true,
   appName: 'Udagram',
-  apiHost: process.env.API_HOST,
+  apiHost: 'http://udagram-api-dev.us-east-1.elasticbeanstalk.com/api/v0',
 };
 
 /*
--- udagram/udagram-frontend/src/environments/environment.ts
+++ udagram/udagram-frontend/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: process.env.API_HOST
+  apiHost: 'http://udagram-api-dev.us-east-1.elasticbeanstalk.com/api/v0'
 };
 
 /*
